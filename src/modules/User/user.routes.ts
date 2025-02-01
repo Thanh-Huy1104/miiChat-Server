e58d0ac5b0a637a83120beed9034108f10004cb5
Jason Rustom <jasonrustom04@gmail.com>
2025-02-01 16:19:42 -0500

    cleanup

--- src/modules/User/user.routes.ts
+++ src/modules/User/user.routes.ts
@@ -32,7 +32,7 @@ router.post("/createUser", async (req, res) => {
     try {
         const user: IUser = await createUser(req.body);
         res.status(200).json(user);
-    } catch (error: unknown) {
+    } catch (error) {
         if (error instanceof Error && error.message == "Username already in use") {
             res.status(400).json({ message: error.message })
         }
